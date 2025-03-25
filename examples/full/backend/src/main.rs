355a409ddea67e485634b456de41c5babf229d3e
asivery <asivery@protonmail.com>
2025-03-25 11:02:33 +0100

    Fix full (backend-inclusive) example

--- examples/full/backend/src/main.rs
+++ examples/full/backend/src/main.rs
@@ -3,14 +3,14 @@ use async_trait::async_trait;
 
 #[tokio::main]
 async fn main() {
-    AppLoad::new(&mut MyBackend).unwrap().run().await.unwrap();
+    AppLoad::new(MyBackend).unwrap().run().await.unwrap();
 }
 
 struct MyBackend;
 
 #[async_trait]
 impl AppLoadBackend for MyBackend {
-    async fn handle_message(&mut self, functionality: &BackendReplier, message: Message) {
+    async fn handle_message(&mut self, functionality: &BackendReplier<MyBackend>, message: Message) {
         match message.msg_type {
             MSG_SYSTEM_NEW_COORDINATOR => {
                 println!("A frontend has connected")
