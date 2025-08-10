92e334a216db7c4b2ff8f218bddb884aea150abb
asivery <asivery@protonmail.com>
2025-08-10 11:38:11 +0200

    Remove support for testing modules when emulating

--- src/main.cpp
+++ src/main.cpp
@@ -13,44 +13,8 @@
 
 #include <dlfcn.h>
 
-void loadTestingModules(){
-    const char *directoryPath = "testing_extensions";
-    DIR *dir;
-    struct dirent *entry;
-    struct stat entryStat;
-
-    dir = opendir(directoryPath);
-    if (dir == nullptr) {
-        CERR << "Unable to open directory" << std::endl;
-        return;
-    }
-
-    while ((entry = readdir(dir)) != nullptr) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-
-        char entryPath[300];
-        snprintf(entryPath, sizeof(entryPath), "%s/%s", directoryPath, entry->d_name);
-        stat(entryPath, &entryStat);
-        if (S_ISREG(entryStat.st_mode)) {
-            void *dl = dlopen((const char *) entryPath, RTLD_NOW);
-            if(!dl) continue;
-            void (*extLoad)() = (void (*)()) dlsym(dl, "_ext_load");
-            if(extLoad) extLoad();
-            CERR << "Loaded test extension " << entryPath << std::endl;
-        }
-    }
-
-    // Close the directory
-    closedir(dir);
-
-}
-
 int main(int argc, char *argv[])
 {
-    loadTestingModules();
-
     QGuiApplication a(argc, argv);
     QQmlApplicationEngine engine;
     appload::library::loadApplications();
